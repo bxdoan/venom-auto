65625359655afc23e4af2ccc2f651590d5c77b51
Doan Bui <bxdoan93@gmail.com>
2023-08-22 21:03:15 +0800

    setup

--- setup.sh
+++ setup.sh
@@ -10,3 +10,4 @@ fi
 rm -rf .venv
 $pipenv sync
 $pipenv run pip install git+https://github.com/bxdoan/dongle-lte-api.git
+$pipenv run pip install -e 'git+https://github.com/jdholtz/undetected-chromedriver.git@29551bd27954dacaf09864cf77935524db642c1b#egg=undetected_chromedriver'
